48ae2271a27b59070e3c381326fc92f6c24caeb2
Louis Midson LAJEANTY <louismidsonlajeanty@gmail.com>
2023-10-31 09:23:10 -0400

    Update Settings for Deployment

--- config/settings.py
+++ config/settings.py
@@ -20,12 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='s$6hs0bf&or@o(jz*l6uf++ejmgtoq7pm&-e8j7i3^3y5bktxt')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST')]
+if not DEBUG:
+    ALLOWED_HOSTS = ['midsonlajeanty.pythonanywhere.com']
+else:
+    ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
